d97ccf7ce160c7a2f6d0c6a82d3b6928a93f50ed
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-13 16:02:28 -0300

    Create food factory

---
+++ tests/utils/factory.ts
@@ -0,0 +1,17 @@
+import faker from 'faker';
+import factory from 'factory-girl';
+
+factory.define(
+  'Food',
+  {},
+  {
+    id: faker.random.number,
+    name: faker.name.title,
+    description: faker.lorem.paragraph,
+    price: () => String(faker.finance.amount()),
+    available: true,
+    image: faker.image.imageUrl,
+  },
+);
+
+export default factory;
