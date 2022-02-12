9b3d9dc137bbffc3392d3f4e68633fb71823c7e7
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-12 09:08:24 -0300

    Update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./jest.setup.js'],
-  moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__mocks__/fileMock.ts',
-  },
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '!src/*.tsx',
+    '!src/routes/*',
+    '!src/styles/*',
+    'src/pages/**/*',
+    'src/components/**/*',
+  ],
+  coverageDirectory: 'tests/coverage',
+  testMatch: ['**/tests/**/*.spec.tsx'],
 };
