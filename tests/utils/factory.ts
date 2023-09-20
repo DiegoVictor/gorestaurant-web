af2d31cb3a4bac31a2c89e9dbc569fe3ef6f4cac
Diego Victor <diegovictorgonzaga@gmail.com>
2023-09-20 09:26:48 -0300

    Update faker usage

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -1,16 +1,16 @@
-import faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 import factory from 'factory-girl';
 
 factory.define(
   'Food',
   {},
   {
-    id: faker.datatype.number,
+    id: faker.number.int,
     name: faker.commerce.productName,
     description: faker.lorem.paragraph,
     price: () => String(faker.finance.amount()),
     available: true,
-    image: faker.image.imageUrl,
+    image: faker.image.url,
   },
 );
 
